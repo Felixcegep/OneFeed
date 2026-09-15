e048cad769e2b7d98daa2765832df5a802282510
felix <6244794@cegepoutaouais.qc.ca>
2026-09-15 16:50:47 -0400

    Cursor: Apply local changes for cloud agent

--- OneFeed/Shared/Utilities/PlatformSupport.swift
+++ OneFeed/Shared/Utilities/PlatformSupport.swift
@@ -76,14 +76,14 @@ extension Color {
         name: String
     ) -> Color {
         #if os(macOS)
-        Color(nsColor: NSColor(name: name, dynamicProvider: { appearance in
+        return Color(nsColor: NSColor(name: name, dynamicProvider: { appearance in
             appearance.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua
                 ? NSColor(srgbRed: dark.0, green: dark.1, blue: dark.2, alpha: 1)
                 : NSColor(srgbRed: light.0, green: light.1, blue: light.2, alpha: 1)
         }))
         #else
-        let _ = name
-        Color(uiColor: UIColor { traits in
+        _ = name
+        return Color(uiColor: UIColor { traits in
             traits.userInterfaceStyle == .dark
                 ? UIColor(red: dark.0, green: dark.1, blue: dark.2, alpha: 1)
                 : UIColor(red: light.0, green: light.1, blue: light.2, alpha: 1)
